a44738f6a237cbc9d161cc63dd068be98468a836
abideditiyaz <abideditiyaz@gmail.com>
2021-01-15 18:09:37 +0700

    grgew

--- src/jscomponent/homeMenu.js
+++ src/jscomponent/homeMenu.js
@@ -1,13 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const CockTail = () => {
   return (
     // <!-- Cocktail
     <div className="sorted">
+      <Link to="/ChartInfo">
       <div className="wrapper-second">
         <h3>CockTail</h3>
         <i className="fas fa-cocktail"></i>
       </div>
+      </Link>
       <div id="Cocktail" className="main-wrapper"></div>
     </div>
   );
@@ -17,10 +20,12 @@ const Shot = () => {
   return (
     // Shot
     <div className="sorted">
+      <Link to="/ChartInfo">
       <div className="wrapper-second">
         <h3>Shot</h3>
         <i className="fas fa-glass-whiskey"></i>
       </div>
+      </Link>
       <div id="Shot" className="main-wrapper"></div>
     </div>
   );
@@ -30,10 +35,12 @@ const OrdinaryDrink = () => {
   return (
     //Ordinary Drink
     <div className="sorted">
+      <Link to="/ChartInfo">
       <div className="wrapper-second">
         <h3>Ordinary Drink</h3>
         <i className="fas fa-glass-martini-alt"></i>
       </div>
+      </Link>
       <div id="OrdinaryDrink" className="main-wrapper"></div>
     </div>
   );
@@ -43,10 +50,12 @@ const CoffeTea = () => {
   return (
     // <!-- Coffe Tea
     <div className="sorted">
+      <Link to="/ChartInfo">
       <div className="wrapper-second">
         <h3>Coffe / Tea</h3>
         <i className="fas fa-coffee"></i>
       </div>
+      </Link>
       <div id="CoffeTea" className="main-wrapper-khusus"></div>
     </div>
   );
@@ -56,10 +65,12 @@ const Surprise = () => {
   return (
     // Surprise
     <div className="sorted">
+      <Link to="/ChartInfo">
       <div className="wrapper-second">
         <h3>Rarely Noticed</h3>
         <i className="fas fa-beer"></i>
       </div>
+      </Link>
       <div id="Surprise" className="main-wrapper-khusus"></div>
     </div>
   );
